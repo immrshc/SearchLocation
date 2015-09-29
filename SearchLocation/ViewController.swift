bea93c1d2675866431edf1566eb1838b6eb0c48b
Shoichi Imamura <syoichiimamura@gmail.com>
2015-09-29 16:41:47 +0900

    Initial Commit

---
+++ SearchLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchLocation
+//
+//  Created by 今村翔一 on 2015/09/29.
+//  Copyright © 2015年 今村翔一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
